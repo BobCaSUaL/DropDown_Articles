12106246aebda51c00626f988018f63f4ca2d8bd
BobCaSUaL <rrt.casula@gmail.com>
2014-08-03 21:41:19 +0200

    Create DropDown_Articles.php

---
+++ DropDown_Articles.php
@@ -0,0 +1,60 @@
+<?php
+/*
+  Plugin Name: Drop-Down Articles
+  Plugin URI: https://github.com/BobCaSUaL/DropDown_Articles/
+  Description: Make cools drop-down text with ShortCode.
+  Author: BobCaSUaL<casual4free(at)gmail.com>
+  Version: 0.1
+  Author URI: https://github.com/BobCaSUaL
+ */
+
+// Use jQuery
+add_action( 'wp_enqueue_scripts', function() {
+	wp_enqueue_script( 'jquery' );
+} );
+
+// The shortcode
+add_shortcode( 'drop-down_article', function ($atts, $content = null) {
+
+	//extract the arguments setting its defaults values
+	extract( shortcode_atts( array(
+		'title'	=> 'noTitle',
+		'wrapperTag' => 'section',
+		'titleTag' => 'h3',
+		'contentTag' => 'p',
+		'wrapperStyle' => '',
+		'titleStyle' => '',
+		'contentStyle' => '',
+		'effect' => 'no',
+		'hidden' => 'yes',
+	), $atts ) );
+	
+	//should the content be hidden by default?
+	$contentStyle .= ($hidden === "yes" || $hidden === "true") ? ' display: none;' : '';
+	
+	//set the chosen effect
+	switch( $effect ) {
+		case 'slide':
+			$effect = 'slideToggle';
+			break;
+		case 'fade':
+			$effect = 'fadeToggle';
+			break;
+		default:
+			$effect = 'toggle';
+	}
+			
+	//the click function for the title
+	$onTitleClick = "jQuery(this).next().$effect()";
+	
+	//the result of the shortcode
+	return 
+<<<HTML
+  <{$wrapperTag} style="{$wrapperStyle}">
+	<{$titleTag} onclick="{$onTitleClick}" style="{$titleStyle}"><a href="#">{$title}</a></{$titleTag}>
+	<{$contentTag} style="{$contentStyle}">
+	  {$content}
+    </{$contentTag}>
+  </{$wrapperTag}>
+HTML;
+} );
